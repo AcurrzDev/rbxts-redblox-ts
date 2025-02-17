abaf72265a4793c0ece4a4e69df56bb3d72e2c81
acurrzdev <everlastingvoyageroblox@gmail.com>
2025-02-17 02:26:01 -0600

    Added Ratelimit

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import Signal from "./Signal";
 import Spawn from "./Spawn";
 import Clock from "./Clock";
 import Future from "./Future"
+import Ratelimit from "./Ratelimit";
 
 export {
     Bin,
     Signal,
     Spawn,
     Clock,
+    Ratelimit,
     Future
 }
