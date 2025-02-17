533ba7465eb5b32eb6e2e72152b9005dca9c48ef
acurrzdev <everlastingvoyageroblox@gmail.com>
2025-02-17 02:27:07 -0600

    Ratelimit

--- out/index.d.ts
+++ out/index.d.ts
@@ -3,4 +3,5 @@ import Signal from "./Signal";
 import Spawn from "./Spawn";
 import Clock from "./Clock";
 import Future from "./Future";
-export { Bin, Signal, Spawn, Clock, Future };
+import Ratelimit from "./Ratelimit";
+export { Bin, Signal, Spawn, Clock, Ratelimit, Future };
